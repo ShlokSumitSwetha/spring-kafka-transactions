001e96c575a990783a2eba4ff632112689419beb
Sumith Ksheerasagar <ksherasagar@gmail.com>
2023-06-11 16:31:10 +0100

    Initial commit

---
+++ transactions-service/src/main/java/pl/piomin/services/transactions/producer/TransactionsProducer.java
@@ -0,0 +1,45 @@
+package pl.piomin.services.transactions.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.piomin.services.common.model.Order;
+import pl.piomin.services.transactions.domain.OrderGroup;
+import pl.piomin.services.transactions.repository.OrderGroupRepository;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class TransactionsProducer {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(TransactionsProducer.class);
+
+    long id = 1;
+    KafkaTemplate<Long, Order> kafkaTemplate;
+    OrderGroupRepository repository;
+
+    public TransactionsProducer(KafkaTemplate<Long, Order> kafkaTemplate,
+                                OrderGroupRepository repository) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.repository = repository;
+    }
+
+    @Transactional("kafkaTransactionManager")
+    public void sendOrderGroup(boolean error) throws InterruptedException {
+        OrderGroup og = repository.save(new OrderGroup("SENT", 10, 0));
+        generateAndSendPackage(error, og.getId());
+    }
+
+    private void generateAndSendPackage(boolean error, Long groupId)
+            throws InterruptedException {
+        for (long i = 0; i < 10; i++) {
+            Order o = new Order(id++, i+1, i+2, 1000, "NEW", groupId);
+            kafkaTemplate.send("transactions", o.getId(), o);
+        }
+    }
+
+}
